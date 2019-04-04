cff21925bb938822f8f863a1fa848fcece0d77c4
cmh <454063072@qq.com>
2019-04-04 10:44:05 +0800

    todo

--- dbController.py
+++ dbController.py
@@ -4,6 +4,14 @@ class MyDataBase():
     client=pymongo.MongoClient(host='localhost',port=27017)
     db=client.emailDB
     mailCollection=db.mail
-
+    questionCollection=db.question
     def storeInDb(self,dic):
+        #一定记得去重复存储
         self.mailCollection.insert_one(dic)
+
+        #如果type是new，在question表里建立新问题
+
+        #如果是track，在question表里
+        #1.如果问题不存在，报错
+        #2.如果存在，在表中追加邮件id
+
